e892b3c90552fdbac59cb4ac32a4ffd477b788d5
Azamat H. Hackimov <azamat.hackimov@gmail.com>
2026-02-27 10:32:24 +0300

    SpeechLst: add quirks resolver
    Some speech.lst files has inconsistencies. This class provides corrections.

--- File/SpeechLst.cpp
+++ File/SpeechLst.cpp
@@ -2,18 +2,93 @@
 /* SPDX-License-Identifier: LGPL-2.1-or-later */
 
 #include <fstream>
+
 #include "SpeechLst.h"
 
 namespace MH2FSB {
 
-std::ostream & operator<<(std::ostream &out, const SpeechLst &c){
+std::map<std::string, std::vector<std::string>> SpeechLstQuirks::m_quirks = //
+    {
+        {
+            "A01_Escape_Asylum", // Add INMATE_1_1
+            {
+                "GENERIC",
+                "PLAYER",
+                "DIRECTOR_LEO",
+                "ASYLUM_1_1",
+                "ASYLUM_2_1",
+                "ASYLUM_3_1",
+                "INMATE_1_1",
+                "INMATE_2_1",
+                "LEGION_6_1",
+                "LEGION_1_1",
+                "LEGION_4_1",
+                "LEO_HUNTER_1_1",
+                "GASMAN_1",
+                "DR_PICKMAN_1",
+                "MALE_VICTIM_1",
+            }},
+        {"A04_SM_Nightclub", // Remove JUDY_1
+         {
+             "GENERIC",
+             "PLAYER",
+             "DIRECTOR_LEO",
+             // "JUDY_1", // Exclude
+             "GIMP_2_1",
+             "FREAK_4_1",
+             "FREAK_1_1",
+             "FREAK_2_1",
+             "GIMP_1_1",
+             "DUNVIC_1_1",
+         }},
+        {"A06_CIA_Trap",  // Remove JOHN_2_1
+         {
+             "GENERIC",
+             "PLAYER",
+             "DIRECTOR_LEO",
+             "BLOODHOUND_2_1",
+             "BLOODHOUND_4_1",
+             "BLOODHOUND_5_1",
+             "ASYLUM_1_1",
+             "LEO_HUNTER_1_1",
+             "ASYLUM_3_1",
+             // "JOHN_2_1",  // Exclude
+             "WATCHDOG_4_1",
+             "WATCHDOG_5_1",
+             "WATCHDOG_1_1",
+             "WATCHDOG_2_1",
+         }},
+        {"A10_Brothel", // Add WATCHDOG_3_1
+         {
+             "GENERIC",
+             "PLAYER",
+             "DIRECTOR_LEO",
+             "JUDY_1",
+             "DANCER_2_1",
+             "RECEPTIONIST_1",
+             "SLEEPER_2_1",
+             "DJ_1_1",
+             "JOHN_2_1",
+             "WATCHDOG_1_1",
+             "WATCHDOG_3_1", // Cut from release
+             "WATCHDOG_4_1",
+         }}};
+
+std::vector<std::string> SpeechLstQuirks::Resolve(const std::string& level_name) {
+  if (m_quirks.contains(level_name)) {
+    return m_quirks[level_name];
+  }
+  return {};
+}
+
+std::ostream &operator<<(std::ostream &out, const SpeechLst &c) {
   for (auto const &entry : c.m_entries) {
     out << entry << "\r\n";
   }
   return out;
 }
 
-std::istream & operator>>(std::istream &in, SpeechLst &c){
+std::istream &operator>>(std::istream &in, SpeechLst &c) {
   std::string line;
   while (std::getline(in, line)) {
     if (!line.empty()) {
@@ -26,4 +101,4 @@ std::istream & operator>>(std::istream &in, SpeechLst &c){
   return in;
 }
 
-}
+} // namespace MH2FSB
--- File/SpeechLst.h
+++ File/SpeechLst.h
@@ -2,21 +2,32 @@
 /* SPDX-License-Identifier: LGPL-2.1-or-later */
 #pragma once
 
+#include <map>
 #include <string>
 #include <vector>
+
 #include "FileContainer.h"
 
 namespace MH2FSB {
 
-class SpeechLst : public FileContainer {
-  public:
-    std::vector<std::string> GetEntries() { return m_entries; }
-
-    friend std::ostream& operator<<(std::ostream& out, const SpeechLst& c);
-    friend std::istream& operator>>(std::istream& in, SpeechLst& c);
-
-  private:
-    std::vector<std::string> m_entries;
+/// Some speech.lst files has inconsistencies. This class provides corrections.
+class SpeechLstQuirks {
+public:
+  static std::vector<std::string> Resolve(const std::string& level_name);
+private:
+  static std::map<std::string, std::vector<std::string>> m_quirks;
 };
 
-}
+class SpeechLst : public FileContainer {
+public:
+  std::vector<std::string> GetEntries() { return m_entries; }
+  void SetEntries(const std::vector<std::string> &entries) { m_entries = entries; }
+
+  friend std::ostream &operator<<(std::ostream &out, const SpeechLst &c);
+  friend std::istream &operator>>(std::istream &in, SpeechLst &c);
+
+private:
+  std::vector<std::string> m_entries;
+};
+
+} // namespace MH2FSB
