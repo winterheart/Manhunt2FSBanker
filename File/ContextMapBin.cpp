03b095e3703214591be2fc2e1df00d945d7658bc
Azamat H. Hackimov <azamat.hackimov@gmail.com>
2026-03-06 13:06:30 +0300

    Update ContextMap info
    Some entries - just guessing.

--- File/ContextMapBin.cpp
+++ File/ContextMapBin.cpp
@@ -3,11 +3,80 @@
 
 #include "ContextMapBin.h"
 
-#include "../binio.h"
+#include "binio.h"
 
 namespace MH2FSB {
 
-std::ostream& operator<<(std::ostream& out, const ContextMapBin& c) {
+/**
+ * Map for speech types.
+ * Format is DD_[(Q|U)_]NAME where
+ * DD - number of speech type in contextmap.bin
+ * Q - (Questionable) just guessed name
+ * U - (Unknown) unknown speech
+ * NAME - name discovered from script engine
+ */
+std::map<uint32_t, std::string> ContextMapResolve::m_context_map = {
+    {UNKNOWN_00, "00_Unknown"},
+    {NEGATIVE_SEARCH_RESULT, "01_Negative_Search_Result"},
+    {DEFINITE_SIGHTING, "02_Definite_Sighting"},
+    {UNKNOWN_03, "03_Q_Killing_Reaction"},
+    {UNKNOWN_04, "04_Q_Execution_Reaction"},
+    {RUN_TO_INVESTIGATE, "05_Run_To_Investigate"},
+    {WALK_TO_INVESTIGATE, "06_Walk_To_Investigate"},
+    {STOP_AND_LISTEN, "07_Stop_And_Listen"},
+    {CURIOSITY_NO_RESULT, "08_Curiosity_No_Result"},
+    {TAUNT_SEARCH, "09_Taunt_Search"},
+    {POSITIVE_TAUNT_SEARCH, "10_Q_Start_Melee_Combat"},
+    {NEGATIVE_TAUNT_SEARCH, "11_Q_Leave_Melee_Combat"},
+    {UNKNOWN_12, "12_Urinating"},
+    {TAUNT_CHASE, "13_Taunt_Chase"},
+    {TAUNT_SHORT, "14_Taunt_Short"},
+    {TAUNT_SAFE_ZONE, "15_Taunt_Safe_Zone"},
+    {TAUNT_BOUNDARY, "16_Taunt_Boundary"},
+    {TAUNT_PLAYER_DEAD, "17_Taunt_Death"},
+    {JOIN_ATTACK, "18_Join_Attack"},
+    {WAIT_ENEMY_ALONE, "19_Wait_Enemy_Alone"},
+    {WAIT_ENEMY_MULTIPLE, "20_Wait_Enemy_Multiple"},
+    {SNEAK_INVESTIGATE, "21_Q_Mumbling"},
+    {WAIT_IN_COVER, "22_Wait_In_Cover"},
+    {SURPRISE, "23_Surprise"},
+    {GREETINGS, "24_Greetings"},
+    {PLAYER_, "25_Player_"},
+    {CLAIM_TERRITORY, "26_Claim_Territory"},
+    {GENERIC_IND, "27_Generic_Ind"},
+    {WHISTLE, "28_Whistle"},
+    {CHAT_STATEMENT, "29_Q_Chat_Statements"}, // Chat_Statements
+    {CHAT_SEARCH, "30_Q_Start_Search"},
+    {CHAT_INVESTIGATE, "31_Q_Start_Investigate"},
+    {UNKNOWN_32, "32_Q_ShoutForHelp"},
+    {SHOUT_FOR_ASSISTANCE, "33_ShoutForAssistance"},
+    {LIGHT_PAIN, "34_Light_Pain"},
+    {MEDIUM_PAIN, "35_Medium_Pain"},
+    {HIGH_PAIN, "36_High_Pain"},
+    {LONG_PAIN, "37_Long_Pain"},
+    {GENERIC_DEATH, "38_Q_Burning_Pain"},
+    {UNKNOWN_39, "39_Q_Generic_Death"},
+    {EXECUTION_DEATH, "40_Q_Execution_Death"},
+    {GRUNT, "41_Grunt"},
+    {NEGATIVE_CHASE_RESULT, "42_Negative_Chase_Result"},
+    {BEGGING_PLEADING, "43_Begging_Pleading"},
+    {DEAD_BODY_SEEN, "44_Dead_Body_Seen"},
+    {UNKNOWN_45, "45_Q_Exhausted_From_Running"},
+    {UNKNOWN_46, "46_Q_Smoking"},
+    {FAILED_SEARCH, "47_Failed_Search"},
+    {CRAWLSPACE_, "48_Crawlspace_"},
+    {JUMP_REACTION, "49_Jump_Reaction"},
+    {CRAWL_REACTION, "50_Crawl_Reaction"},
+    {CHAT_QUESTION, "51_Chat_Question"},
+    {CHAT_POSITIVE, "52_Chat_Positive"},
+    {CHAT_NEGATIVE, "53_Chat_Negative"},
+    {FLARE_DEATH, "54_Specific_3"},
+    {GASCAN_DEATH, "55_Specific_4"},
+    {UNKNOWN_56, "56_Q_Vomiting"},
+    {UNKNOWN_57, "57_Q_Using_Vending_Machine"},
+};
+
+std::ostream &operator<<(std::ostream &out, const ContextMapBin &c) {
   UTILS::bin_write(out, c.m_bank_name, 32);
   for (auto i : c.m_speech_entries) {
     UTILS::bin_write(out, i);
@@ -15,7 +84,7 @@ std::ostream& operator<<(std::ostream& out, const ContextMapBin& c) {
   return out;
 }
 
-std::istream& operator>>(std::istream& in, ContextMapBin& c) {
+std::istream &operator>>(std::istream &in, ContextMapBin &c) {
   UTILS::bin_read(in, c.m_bank_name, 32);
   for (auto &i : c.m_speech_entries) {
     UTILS::bin_read(in, i);
@@ -23,4 +92,4 @@ std::istream& operator>>(std::istream& in, ContextMapBin& c) {
   return in;
 }
 
-} // MH2FSB
+} // namespace MH2FSB
--- File/ContextMapBin.h
+++ File/ContextMapBin.h
@@ -4,6 +4,8 @@
 
 #include <array>
 #include <cstdint>
+#include <map>
+
 #include "FileContainer.h"
 
 namespace MH2FSB {
@@ -41,75 +43,91 @@ Specific_7
 Specific_8
  */
 
-enum class ContextNames {
-  UNKNOWN_00,
-  NEGATIVE_SEARCH_RESULT, // Negative_Search_Result?
-  DEFINITE_SIGHTING, // Definite_Sighting
-  UNKNOWN_03,
-  UNKNOWN_04,
-  RUN_TO_INVESTIGATE, //
-  WALK_TO_INVESTIGATE, // Walk_To_Investigate
-  STOP_AND_LISTEN, // Stop_And_Listen
-  CURIOSITY_NO_RESULT, // Curiosity_No_Result
-  TAUNT_SEARCH, // Taunt_Search
-  POSITIVE_TAUNT_SEARCH,
-  NEGATIVE_TAUNT_SEARCH,
-  UNKNOWN_12,
-  TAUNT_CHASE, // Taunt_Chase
-  TAUNT_SHORT,
-  TAUNT_SAFE_ZONE, //
-  TAUNT_BOUNDARY,
-  TAUNT_PLAYER_DEAD, // TAUNT_DEATH?
-  JOIN_ATTACK, // Join_Attack
-  WAIT_ENEMY_ALONE,
-  WAIT_ENEMY_MULTIPLE,
-  SNEAK_INVESTIGATE,
-  WAIT_IN_COVER,
-  SURPRISE,
-  GREETINGS,
-  PLAYER_,
-  CLAIM_TERRITORY,
-  GENERIC_IND,
-  WHISTLE,
-  CHAT_STATEMENT, //
-  CHAT_SEARCH,
-  CHAT_INVESTIGATE,
-  UNKNOWN_32,
-  SHOUT_FOR_ASSISTANCE, // ShoutForAssistance
-  LIGHT_PAIN, // Light_Pain
-  MEDIUM_PAIN, // Medium_Pain
-  HIGH_PAIN, // High_Pain
-  LONG_PAIN, // Long_Pain
-  GENERIC_DEATH, // Generic_Death
-  UNKNOWN_39,
-  EXECUTION_DEATH, // Execution_Death
-  GRUNT, // Grunt
-  NEGATIVE_CHASE_RESULT, //
-  BEGGING_PLEADING,
-  DEAD_BODY_SEEN,
-  UNKNOWN_45,
-  UNKNOWN_46,
-  FAILED_SEARCH,
-  CRAWLSPACE_,
-  JUMP_REACTION,
-  CRAWL_REACTION, //
-  CHAT_QUESTION,
-  CHAT_POSITION,
-  CHAT_NEGATIVE,
-  FLARE_DEATH, // Flare_Death
-  GASCAN_DEATH,
-  UNKNOWN_56,
-  UNKNOWN_57,
+enum ContextNames {
+  UNKNOWN_00,             // Not seen
+  NEGATIVE_SEARCH_RESULT, // Negative_Search_Result? 9
+  DEFINITE_SIGHTING,      // Definite_Sighting? 12
+  UNKNOWN_03,             // Only for DIRECTOR_LEO. Probably killing reaction?
+  UNKNOWN_04,             // Only for DIRECTOR_LEO. Probably execution reaction?
+  RUN_TO_INVESTIGATE,     // Run_To_Investigate? 13
+  WALK_TO_INVESTIGATE,    // Walk_To_Investigate? 14
+  STOP_AND_LISTEN,        // Stop_And_Listen? 15
+  CURIOSITY_NO_RESULT,    // Curiosity_No_Result? 16
+  TAUNT_SEARCH,           // Taunt_Search? 17
+  POSITIVE_TAUNT_SEARCH,  // Start_Melee_Combat? Taunt_Combat?
+  NEGATIVE_TAUNT_SEARCH,  // Disengage from melee battle?
+  UNKNOWN_12,             // Urinating?
+  TAUNT_CHASE,            // Taunt_Chase 22
+  TAUNT_SHORT,            //
+  TAUNT_SAFE_ZONE,        // Taunt_Safe_Zone? 24
+  TAUNT_BOUNDARY,         //
+  TAUNT_PLAYER_DEAD,      // TAUNT_DEATH? Taunt_Death? 25
+  JOIN_ATTACK,            // Join_Attack? 26
+  WAIT_ENEMY_ALONE,       // Enemy_Runs_In_SafeZone? 27
+  WAIT_ENEMY_MULTIPLE,    //
+  SNEAK_INVESTIGATE,      // Mumbling? 29
+  WAIT_IN_COVER,          //
+  SURPRISE,               // Chat_Statements? 31
+  GREETINGS,              //
+  PLAYER_,                // On failed to hide?
+  CLAIM_TERRITORY,        //
+  GENERIC_IND,            //
+  WHISTLE,                //
+  CHAT_STATEMENT,         //
+  CHAT_SEARCH,            // Start_Search?
+  CHAT_INVESTIGATE,       // Start_Investigate?
+  UNKNOWN_32,             // ShoutForHelp? 40
+  SHOUT_FOR_ASSISTANCE,   // ShoutForAssistance 41
+  LIGHT_PAIN,             // Light_Pain 42
+  MEDIUM_PAIN,            // Medium_Pain 43
+  HIGH_PAIN,              // High_Pain 44
+  LONG_PAIN,              // Long_Pain 45
+  GENERIC_DEATH,          // Generic_Death Burning_Pain? 46
+  UNKNOWN_39,             // Scream of pain/agony for PLAYER, rage and triumph for DIRECTOR_LEO Generic_Death? 47
+  EXECUTION_DEATH,        // Execution_Death? 48 Burning_Death? 49
+  GRUNT,                  // Grunt 51
+  NEGATIVE_CHASE_RESULT,  // Negative_Chase_Result
+  BEGGING_PLEADING,       //
+  DEAD_BODY_SEEN,         //
+  UNKNOWN_45,             // Exhausted from running?
+  UNKNOWN_46,             // Smoking?
+  FAILED_SEARCH,          //
+  CRAWLSPACE_,            //
+  JUMP_REACTION,          //
+  CRAWL_REACTION,         // Crawl_Reaction? 58
+  CHAT_QUESTION,          // Negative_Chase_Result_Argue? 59
+  CHAT_POSITIVE,          // Positive answer in chat Specific_1 60
+  CHAT_NEGATIVE,          // Negative answer in chat Specific_2 61
+  FLARE_DEATH,            // Specific_3 / Flare_Death 62
+  GASCAN_DEATH,           // Specific_4 63
+  UNKNOWN_56,             // Vomiting? Specific_5 64
+  UNKNOWN_57,             // Interacting with vending machine Specific_6 65
+};
+
+class ContextMapResolve {
+public:
+  static std::string getContextMapName(uint32_t context) {
+    if (m_context_map.contains(context)) {
+      return m_context_map[context];
+    }
+    return "";
+  }
+
+private:
+  static std::map<uint32_t, std::string> m_context_map;
 };
 
 class ContextMapBin : public FileContainer {
-  public:
-    friend std::ostream& operator<<(std::ostream& out, const ContextMapBin& c);
-    friend std::istream& operator>>(std::istream& in, ContextMapBin& c);
+public:
+  std::string getBankName() { return m_bank_name; }
+  std::array<uint32_t, 58> &GetEntries() { return m_speech_entries; }
 
-  private:
-    std::string m_bank_name;
-    std::array<uint32_t, 58> m_speech_entries;
+  friend std::ostream &operator<<(std::ostream &out, const ContextMapBin &c);
+  friend std::istream &operator>>(std::istream &in, ContextMapBin &c);
+
+private:
+  std::string m_bank_name;
+  std::array<uint32_t, 58> m_speech_entries;
 };
 
-} // MH2FSB
+} // namespace MH2FSB
